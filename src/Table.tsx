7efc6b44ee00673cc38e8145928372af6d3ded2e
WangCe <halowang1991@gmail.com>
2020-02-28 21:22:44 +0800

    fix type define for tableAlertRender (#168)

--- src/Table.tsx
+++ src/Table.tsx
@@ -231,7 +231,7 @@ export interface ProTableProps<T, U extends { [key: string]: any }>
    * 自定义 table 的 alert
    * 设置或者返回false 即可关闭
    */
-  tableAlertRender?: (keys: (string | number)[], rows: T[]) => React.ReactNode;
+  tableAlertRender?: ((keys: (string | number)[], rows: T[]) => React.ReactNode) | false;
   /**
    * 自定义 table 的 alert 的操作
    * 设置或者返回false 即可关闭
