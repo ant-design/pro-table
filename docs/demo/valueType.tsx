475fe38eccba54d2a3845705c98a2f08265940f8
xiaofei <niexiaofei1988@users.noreply.github.com>
2020-03-08 23:08:01 +0800

    feature: add valueType percent (#202)
    * feature: add valueType percent
    
    * feature: fix file path
    
    * Update docs/valueType.md
    
    Co-Authored-By: 陈帅 <qixian.cs@outlook.com>
    
    Co-authored-by: 陈帅 <qixian.cs@outlook.com>

--- docs/demo/valueType.tsx
+++ docs/demo/valueType.tsx
@@ -18,6 +18,7 @@ export interface TableListItem {
   createdAt: number;
   progress: number;
   money: number;
+  percent: number | string;
   createdAtRange: number[];
 }
 const tableListDataSource: TableListItem[] = [];
@@ -35,6 +36,10 @@ for (let i = 0; i < 10; i += 1) {
     ],
     money: Math.floor(Math.random() * 2000) * i,
     progress: Math.ceil(Math.random() * 100) + 1,
+    percent:
+      Math.random() > 0.5
+        ? (i + 1) * 10 + Math.random()
+        : -((i + 1) * 10 + Math.random()).toFixed(5),
   });
 }
 
@@ -123,6 +128,17 @@ const columns: ProColumns<TableListItem>[] = [
     width: 120,
     dataIndex: 'updatedAt',
     valueType: 'time',
+  },
+  {
+    title: '百分比',
+    key: 'percent',
+    width: 120,
+    dataIndex: 'percent',
+    valueType: () => ({
+      type: 'percent',
+      showSymbol: true,
+      precision: 3,
+    }),
   },
   {
     title: '操作',
