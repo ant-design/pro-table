96f1a65be74260b05acfa94d924aab46376a2efc
kkcy <rickykkcy@gmail.com>
2020-03-26 19:08:40 +0800

    Add missing locale file

---
+++ src/locale/ms_MY.tsx
@@ -0,0 +1,33 @@
+export default {
+  tableFrom: {
+    search: 'Cari',
+    reset: 'Menetapkan semula',
+    submit: 'Hantar',
+    collapsed: 'Kembang',
+    expand: 'Kuncup',
+    inputPlaceholder: 'Sila masuk',
+    selectPlaceholder: 'Sila pilih',
+  },
+  alert: {
+    clear: 'Padam',
+  },
+  tableToolBar: {
+    leftPin: 'Pin ke kiri',
+    rightPin: 'Pin ke kanan',
+    noPin: 'Tidak pin',
+    leftFixedTitle: 'Tetap ke kiri',
+    rightFixedTitle: 'Tetap ke kanan',
+    noFixedTitle: 'Tidak Tetap',
+    reset: 'Menetapkan semula',
+    columnDisplay: 'Lajur',
+    columnSetting: 'Settings',
+    fullScreen: 'Full Screen',
+    exitFullScreen: 'Keluar Full Screen',
+    reload: 'Muat Semula',
+    density: 'Densiti',
+    densityDefault: 'Biasa',
+    densityLarger: 'Besar',
+    densityMiddle: 'Tengah',
+    densitySmall: 'Kecil',
+  },
+};
