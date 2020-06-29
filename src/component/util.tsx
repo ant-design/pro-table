dbe76830bef782012ade2fd82ded843a5b9f9f5e
chenshuai2144 <qixian.cs@outlook.com>
2020-06-29 22:46:37 +0800

    fix lint

--- src/component/util.tsx
+++ src/component/util.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import React, { ReactNode, useEffect, useRef, ReactText, DependencyList, useCallback } from 'react';
 import isEqual from 'lodash.isequal';
 import { DataIndex } from 'rc-table/lib/interface';
