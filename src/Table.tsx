7cbc02e9a151b281bc72a6358343b3a22e22537b
韩贵和 <592029487@qq.com>
2020-07-03 12:46:45 +0800

    fix: 修复可能造成dataSource丢失的问题

--- src/Table.tsx
+++ src/Table.tsx
@@ -53,25 +53,25 @@ export interface ColumnsState {
 
 export type ValueEnumObj = {
   [key: string]:
-  | {
-    text: ReactNode;
-    status: StatusType;
-  }
-  | ReactNode;
+    | {
+        text: ReactNode;
+        status: StatusType;
+      }
+    | ReactNode;
 };
 
 export type ValueEnumMap = Map<
   React.ReactText,
   | {
-    text: ReactNode;
-    status: StatusType;
-  }
+      text: ReactNode;
+      status: StatusType;
+    }
   | ReactNode
 >;
 
 export interface ProColumnType<T = unknown>
   extends Omit<ColumnType<T>, 'render' | 'children' | 'title' | 'filters'>,
-  Partial<Omit<FormItemProps, 'children'>> {
+    Partial<Omit<FormItemProps, 'children'>> {
   index?: number;
   title?: ReactNode | ((config: ProColumnType<T>, type: ProTableTypes) => ReactNode);
   /**
@@ -280,19 +280,19 @@ export interface ProTableProps<T, U extends { [key: string]: any }>
    * 设置或者返回false 即可关闭
    */
   tableAlertRender?:
-  | ((props: {
-    intl: IntlType;
-    selectedRowKeys: (string | number)[];
-    selectedRows: T[];
-  }) => React.ReactNode)
-  | false;
+    | ((props: {
+        intl: IntlType;
+        selectedRowKeys: (string | number)[];
+        selectedRows: T[];
+      }) => React.ReactNode)
+    | false;
   /**
    * 自定义 table 的 alert 的操作
    * 设置或者返回false 即可关闭
    */
   tableAlertOptionRender?:
-  | ((props: { intl: IntlType; onCleanSelected: () => void }) => React.ReactNode)
-  | false;
+    | ((props: { intl: IntlType; onCleanSelected: () => void }) => React.ReactNode)
+    | false;
 
   rowSelection?: TableProps<T>['rowSelection'] | false;
 
@@ -495,18 +495,11 @@ const genColumnList = <T, U = {}>(
   columnEmptyText?: ColumnEmptyText,
 ): (ColumnsType<T>[number] & { index?: number })[] =>
   (columns
-    .map((item) => {
-      const { title } = item;
-      return {
-        ...item,
-        title: title && typeof title === 'function' ? title(item, 'table') : title,
-        valueEnum: ObjToMap(item.valueEnum),
-      };
-    })
     .map((item, columnsIndex) => {
-      const { key, dataIndex, filters = [] } = item;
+      const { key, title, dataIndex, filters = [] } = item;
       const columnKey = genColumnKey(key, dataIndex, columnsIndex);
       const config = columnKey ? map[columnKey] || { fixed: item.fixed } : { fixed: item.fixed };
+      const valueEnum = ObjToMap(item.valueEnum);
       const tempColumns = {
         onFilter: (value: string, record: T) => {
           let recordElement = get(record, item.dataIndex || '');
@@ -518,11 +511,14 @@ const genColumnList = <T, U = {}>(
         },
         index: columnsIndex,
         ...item,
-        filters: filters === true
-          ? parsingValueEnumToArray(item.valueEnum).filter(
-            (valueItem) => valueItem && valueItem.value !== 'all',
-          )
-          : filters,
+        title: title && typeof title === 'function' ? title(item, 'table') : title,
+        valueEnum,
+        filters:
+          filters === true
+            ? parsingValueEnumToArray(valueEnum).filter(
+                (valueItem) => valueItem && valueItem.value !== 'all',
+              )
+            : filters,
         ellipsis: false,
         fixed: config.fixed,
         width: item.width || (item.fixed ? 200 : undefined),
@@ -543,9 +539,9 @@ const genColumnList = <T, U = {}>(
       return tempColumns;
     })
     .filter((item) => !item.hideInTable) as unknown) as ColumnsType<T>[number] &
-  {
-    index?: number;
-  }[];
+    {
+      index?: number;
+    }[];
 
 /**
  * 🏆 Use Ant Design Table like a Pro!
@@ -583,7 +579,7 @@ const ProTable = <T extends {}, U extends object>(
     defaultClassName,
     formRef,
     type = 'table',
-    onReset = () => { },
+    onReset = () => {},
     columnEmptyText = '-',
     ...rest
   } = props;
@@ -940,8 +936,8 @@ const ProTable = <T extends {}, U extends object>(
                     const { name = 'keyword' } =
                       options.search === true
                         ? {
-                          name: 'keyword',
-                        }
+                            name: 'keyword',
+                          }
                         : options.search;
                     setFormSearch({
                       [name]: keyword,
